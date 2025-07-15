585cd9f3f2d11f73f00578f9d7e89968169a8bfc
shayankhannn <shayannkhannn007@gmail.com>
2025-07-15 00:21:17 -0700

    var

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
-echo"hi";
+
+$variable = "variable";
+$name = "name";
+echo "my name is $name";
+echo $variable;
+
+echo "<br>";
+// int 
+$age = 32;
+echo "<br>";
+echo "my age $age";
+echo "<br>";
+
+$gpa = 2.5;
+echo "my gpa 2.5";
+
+echo "<br>";
+$price = 4.99;
+
+echo "price \${$price}"
 
 ?>
