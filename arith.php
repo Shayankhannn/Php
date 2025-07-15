e9e73c723f90d90570b6e06ea98c2cb4edb51ca1
shayankhannn <shayannkhannn007@gmail.com>
2025-07-15 01:02:43 -0700

    get post

---
+++ arith.php
@@ -0,0 +1,39 @@
+<?php
+
+$x = 10;
+$y = 3;
+
+$z = $x + $y;
+echo "$z";
+
+$r = $x ** $y;
+echo "$r <br>";
+
+// increment/decrement 
+
+$counter = 0;
+// $counter++;
+$counter+=2;
+echo"$counter";
+
+// operator precedence 
+
+// ()
+//  ** 
+// * / % 
+//  + - 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,31 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- method="get" -->
+    <!-- <form action="index.php" method="post">
+    <label for="">UserName:</label><br>
+    <input type="text" name="username"/><br>
+    <label for="">Password:</label><br>
+    <input type="password" name="password"/><br>
+    <button type="submit">Log in</button>
+</form> -->
+
+    <form action="index.php" method="get">
+        <label for="">Quantity</label><br>
+        <input type="text" name="quantity"><br>
+        <input type="submit" name="total" value="submit">
+    </form>
+
+</body> 
+</html>
+
+
+
 <?php
 
-$x = 10;
-$y = 3;
+// echo $_GET["username"] . "<br>";
+// echo $_GET["password"];
 
-$z = $x + $y;
-echo "$z";
+// echo $_POST["username"] . "<br>";
+// echo $_POST["password"];
 
-$r = $x ** $y;
-echo "$r <br>";
+$item = "Pizza";
 
-// increment/decrement 
+$price = 5.98;
 
-$counter = 0;
-// $counter++;
-$counter+=2;
-echo"$counter";
+$quantity = $_GET["quantity"];
+$total = null;
 
-// operator precedence 
+$total = $price * $quantity;
 
-// ()
-//  ** 
-// * / % 
-//  + - 
-
-
-
-
+echo"You have ordered $quantity x $item <br>";
+echo "Your total is $total";
 
 
 
