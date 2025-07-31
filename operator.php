2b7cb9c9b0ceece3db7de3320a7ea4ba3d800b78
shayankhannn <shayannkhannn007@gmail.com>
2025-07-31 00:12:45 -0700

    operator

--- operator.php
+++ operator.php
@@ -4,12 +4,24 @@
 $a = "Hello";
 $b = "World";
 
-$c = $a . $b; // concatenate a and b
+$c = $a . " " . $b; // concatenate a and b
 
 
+$a = 4;
+$b = 4;
+$c = 4;
+$d = 4;
 
 
+// and -- && 
+if($a == $b and $c == $d){
+    echo "this statement is true";
+}
 
+// or -- || 
+if($a == $b or $c == $d){
+    echo "this statement is true";
+}
 
 
 
