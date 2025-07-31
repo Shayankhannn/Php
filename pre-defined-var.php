4e1e4effb652f2211f68a5a1bb4960f26bb9fb6e
shayankhannn <shayannkhannn007@gmail.com>
2025-07-30 23:51:04 -0700

    pre define var and form things

---
+++ pre-defined-var.php
@@ -0,0 +1,43 @@
+<?php
+
+
+echo $_SERVER["DOCUMENT_ROOT"] . "<br>";
+echo $_SERVER["PHP_SELF"] . "<br>";
+echo $_SERVER["SERVER_NAME"] . "<br>";
+echo $_SERVER["REQUEST_METHOD"] . "<br>";
+echo $_GET["name"] . "<br>";
+echo $_REQUEST["name"] . "<br>";
+echo $_FILES["name"] . "<br>"; // get dagta of file uploaded to websigte
+echo $_COOKIE["name"] . "<br>";
+$_SESSION["username"] = "DAni";
+echo $_SESSION["username"] . "<br>";
+echo $_ENV["username"] . "<br>";
+
+
+
+
+// form related
+
+var_dump($_SERVER["REQUEST_METHOD"]);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $firstName = htmlspecialchars($_POST["name"]); // sanitize code
+    // htmlentities() -- same as htmlspecial char but it take all sort of char and convert it to html entities
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ radio.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post " action="index.php">
+        <input type="radio" value="visa" name="card">
+        <input type="radio" value="mastercard" name="card">
+        <input type="radio" value="paypal" name="card">
+        <input type="submit" value="submit" name="confirm">
+    </form>
+</body>
+</html>
+
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
