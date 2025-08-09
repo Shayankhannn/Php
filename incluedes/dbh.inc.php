abf5108323a63c284a45b7cec6a352972f35c3e2
shayankhannn <shayannkhannn007@gmail.com>
2025-08-08 23:55:51 -0700

    db connecction with php

---
+++ incluedes/dbh.inc.php
@@ -0,0 +1,30 @@
+<!-- in pure php file we dont close php tag to save ourself from creating more damage by accidently writing below the closing tag  -->
+
+<?php
+// dsn -- database source name
+$dsn = "mysql:host=localhost;dbname=php";
+$dbusername = "root";
+$dbpassword = "";
+
+// three ways of connection 
+
+// mysql connection -- never do it -- security things
+// mysqli connection -- you can use it means mysql improve -- very good for mysql databases 
+// pdo connection -- php data object -- more flexible with databases out there 
+
+ try {
+    $pdo = new PDO($dsn,$dbusername,$dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+ } catch (PDOException $e) {
+    echo "failed connection:" . $e->getMessage();
+ } 
+
+
+
+
+
+
+
+
+
+
