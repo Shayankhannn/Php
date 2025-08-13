60fa5739b71991b0f57c4575bc34898a71bde841
shayankhannn <shayannkhannn007@gmail.com>
2025-08-13 00:26:00 -0700

    project -- error functions

--- project/includes/signup.inc.php
+++ project/includes/signup.inc.php
@@ -7,7 +7,27 @@ if($_SERVER["REQUEST_METHOD"] === 'POST'){
    $pwd =  $_POST["pwd"];
    
    try {
-    //code...
+    // first come connectiona then model then controller in a situation if we need view it comes in between model and controller
+      require_once 'dbh.inc.php';
+      require_once 'signup_model.inc.php';
+      require_once 'signup_controller.inc.php';
+
+      
+
+      if (is_input_empty($username,$pwd, $email)) {
+         # code...
+      }
+      if (is_email_valid( $email)) {
+         # code...
+      }
+      if (is_username_taken( $pdo , $username)) {
+         # code...
+      }
+      if (is_email_registered( $pdo , $email)) {
+         # code...
+      }
+      
+
    } catch (PDOException $e) {
     die("query failed--:" . $e->getMessage());
    }
