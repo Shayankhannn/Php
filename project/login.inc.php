57387204b7d19983e6745fa0903a8eb73b836ae5
shayankhannn <shayannkhannn007@gmail.com>
2025-08-16 01:00:52 -0700

    login

---
+++ project/login.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+  $username =  $_POST["username"];  
+   $pwd =  $_POST["pwd"];
+
+   try {
+    //code...
+   
+   } catch (PDOException $e) {
+    die("query failed--:" . $e->getMessage());
+   }
+}else{
+    header("Location : ../index.php");
+    die();
+}
